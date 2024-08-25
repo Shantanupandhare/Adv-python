d00ddb3d099e9cc5408d4110d3cbe96de938d4a3
Shantanupandhare <shantanupandhare99@gmail.com>
2024-08-25 11:15:45 +0530

    Second Commit

---
+++ test.py
@@ -0,0 +1,16 @@
+def simple_interst(p,n,r):
+    """
+    This will calculte simple interest
+    """
+    I= (p*n*r)/100
+    A= p+I
+    return I, A
+
+"""
+__main__ will refer to current file
+"""
+
+if __name__ == "__main__":
+    I1,A1 = simple_interst(p=57000,n=5,r= 7.5)
+    print(f"Simple Intrest : {I1:.2f}")
+    print(f'Amount : {A1:.2f}')
